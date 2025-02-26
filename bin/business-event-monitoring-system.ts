4d7b59ae8b64076be2e95f1b135658856650f0d0
Grey Newell <newellgr@amazon.com>
2025-02-26 02:01:26 +0000

    feat: Create empty stacks

---
+++ bin/business-event-monitoring-system.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+
+import { EventRouterStack } from './event-router-stack';
+import { EventProducerStack } from './event-producer-stack';
+import { EventConsumerStack } from './event-consumer-stack';
+
+const app = new cdk.App();
+
+const usEast2StackProps: cdk.StackProps = {
+    env: { account: '619071328044', region: 'us-east-2'}
+};
+
+// us-east-2
+new EventRouterStack(app, 'event-router-stack', usEast2StackProps);
+new EventProducerStack(app, 'event-producer-stack', usEast2StackProps);
+new EventConsumerStack(app, 'event-consumer-stack', usEast2StackProps);
