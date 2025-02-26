d9fba625fdaa13e0b621bac6816450347991d76a
Grey Newell <newellgr@amazon.com>
2025-02-26 02:07:26 +0000

    feat: EventRouter construct

--- bin/event-router-stack.ts
+++ bin/event-router-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { EventRouter } from '../lib/eventRouter';
 
 export class EventRouterStack extends cdk.Stack {
+  router: EventRouter;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
+
+    this.router = new EventRouter(this, id + 'Router');
   }
 }
---
+++ lib/eventRouter.ts
@@ -0,0 +1,36 @@
+import { Construct } from "constructs";
+import { EventBus, Rule } from "aws-cdk-lib/aws-events";
+import { Topic, TopicProps } from "aws-cdk-lib/aws-sns";
+import { ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { Stack } from "aws-cdk-lib";
+
+export type EventRouterProps = {
+
+}
+
+export class EventRouter extends Construct {
+    targets: Topic[];
+    rules: Rule[];
+    bus: EventBus;
+
+    constructor(scope: Construct, id: string, props: EventRouterProps = {}) {
+        super(scope, id);
+
+        this.bus = new EventBus(scope, 'event-choreographer', {
+            eventBusName: 'event-choreographer'
+        });
+    }
+
+    addRoutingTarget(stack: Stack, name: string, props: TopicProps) {
+        const routingTarget = new Topic(stack, name + 'Topic', props);
+        const rule = new Rule(stack, name + 'Rule', {
+            targets: [routingTarget.bindAsNotificationRuleTarget],
+            eventBus: this.bus
+        });
+
+        routingTarget.grantPublish(new ServicePrincipal('events.amazonaws.com'));
+
+        this.targets.push(routingTarget);
+        this.rules.push(rule);
+    }
+}
